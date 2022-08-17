c2d815bfde85487ca2bcb02f82722871b4c78910
kamilsmr <kamils6035@gmail.com>
2022-08-17 23:35:15 +0300

    prompt

---
+++ 08172022javascript/app.js
@@ -0,0 +1,10 @@
+
+console.log("welcome to javascript");
+
+let name = prompt('adinizi giriniz');
+
+let sonuc = confirm('emin misiniz');
+console.log(sonuc);
+
+console.log('adiniz : ' + name +' dir');
+console.log(`adiniz : ${name} dir`)
