58e0f0c44f598f3963f0e90d18cbe6aea5563429
kamilsmr <kamils6035@gmail.com>
2022-09-03 10:20:10 +0300

    start

--- 01092022/app.js
+++ 01092022/app.js
@@ -1,7 +1,50 @@
 // 1-10 arası sayı yazdırma
-for(var i = 1; i <= 10; i++)
-{
-    console.log(i)
-}
+// for(let i = 1; i <= 10; i++)
+// {
+//     console.log(i)
+// }
 
-//
+//3ün katları, 5inkatları
+
+// for(let i = 1; i <= 10; i++)
+// {
+//     let row = "7 * " + i + " = " + 7 * i;
+//     console.log(row);
+// }
+
+// 7in katları
+// for(let i = 1; i <= 10; i++)
+// {
+//     let row =7 * i;
+//     console.log(row);
+// }
+
+// for(let i = 1; i <= 10; i++)
+// {
+//     let row =3 * i;
+//     console.log(row);
+// }
+
+
+// for (let i = 4; i<=20; i++){
+//     let formul = (i*2-5)*4
+//     console.log(formul)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
